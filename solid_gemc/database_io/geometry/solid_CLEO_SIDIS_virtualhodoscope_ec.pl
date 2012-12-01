e1e9cdec4bb0814352a493d29c18863287fe93e9
zwzhao <zwzhao@jlab.org>
2012-12-01 18:23:44 +0000

    add preshower virtual hodoscope

--- solid_gemc/database_io/geometry/solid_CLEO_SIDIS_virtualhodoscope_ec.pl
+++ solid_gemc/database_io/geometry/solid_CLEO_SIDIS_virtualhodoscope_ec.pl
@@ -28,7 +28,7 @@ sub make_forwardangle_front
  $detector{"name"}        = "$DetectorName\_forwardangle_front";
  $detector{"mother"}      = "root";
  $detector{"description"} = $detector{"name"};
- $detector{"pos"}         = "0*cm 0*cm 404*cm";
+ $detector{"pos"}         = "0*cm 0*cm 403*cm";
  $detector{"rotation"}    = "0*deg 0*deg 0*deg";
  $detector{"color"}       = "CC6633";
  $detector{"type"}        = "Cons";
@@ -53,6 +53,68 @@ sub make_forwardangle_front
  print_det(\%detector, $file);
 }
 make_forwardangle_front();
+
+sub make_forwardangle_shield
+{
+ $detector{"name"}        = "$DetectorName\_forwardangle_shield";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 403.8*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 100;
+  my $Rmax1 = 215;
+  my $Rmin2 = 100;
+  my $Rmax2 = 215;
+  my $Dz    = 0.6;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "Lead";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "no";
+ $detector{"hit_type"}    = "";
+ $detector{"identifiers"} = "";
+ print_det(\%detector, $file);
+}
+make_forwardangle_shield();
+
+sub make_forwardangle_middle
+{
+ $detector{"name"}        = "$DetectorName\_forwardangle_middle";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm 404.6*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 100;
+  my $Rmax1 = 215;
+  my $Rmin2 = 100;
+  my $Rmax2 = 215;
+  my $Dz    = 0.1;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "Vacuum";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "FLUX";
+ $detector{"hit_type"}    = "FLUX";
+ $detector{"identifiers"} = "id manual 3120000";
+ print_det(\%detector, $file);
+}
+make_forwardangle_middle();
 
 sub make_forwardangle_inner
 {
@@ -80,7 +142,7 @@ sub make_forwardangle_inner
  $detector{"style"}       = 0;
  $detector{"sensitivity"} = "FLUX";
  $detector{"hit_type"}    = "FLUX";
- $detector{"identifiers"} = "id manual 3120000";
+ $detector{"identifiers"} = "id manual 3130000";
  print_det(\%detector, $file);
 }
 make_forwardangle_inner();
@@ -111,17 +173,18 @@ sub make_forwardangle_rear
  $detector{"style"}       = 0;
  $detector{"sensitivity"} = "FLUX";
  $detector{"hit_type"}    = "FLUX";
- $detector{"identifiers"} = "id manual 3130000";
+ $detector{"identifiers"} = "id manual 3140000";
  print_det(\%detector, $file);
 }
 make_forwardangle_rear();
+
 
 sub make_largeangle_front
 {
  $detector{"name"}        = "$DetectorName\_largeangle_front";
  $detector{"mother"}      = "root";
  $detector{"description"} = $detector{"name"};
- $detector{"pos"}         = "0*cm 0*cm -66*cm";
+ $detector{"pos"}         = "0*cm 0*cm -67*cm";
  $detector{"rotation"}    = "0*deg 0*deg 0*deg";
  $detector{"color"}       = "CC6633";
  $detector{"type"}        = "Cons";
@@ -146,6 +209,68 @@ sub make_largeangle_front
  print_det(\%detector, $file);
 }
 make_largeangle_front();
+
+sub make_largeangle_shield
+{
+ $detector{"name"}        = "$DetectorName\_largeangle_shield";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm -66.2*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 75;
+  my $Rmax1 = 140;
+  my $Rmin2 = 75;
+  my $Rmax2 = 140;
+  my $Dz    = 0.6;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "Lead";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "no";
+ $detector{"hit_type"}    = "";
+ $detector{"identifiers"} = "";
+ print_det(\%detector, $file);
+}
+make_largeangle_shield();
+
+sub make_largeangle_middle
+{
+ $detector{"name"}        = "$DetectorName\_largeangle_middle";
+ $detector{"mother"}      = "root";
+ $detector{"description"} = $detector{"name"};
+ $detector{"pos"}         = "0*cm 0*cm -65.4*cm";
+ $detector{"rotation"}    = "0*deg 0*deg 0*deg";
+ $detector{"color"}       = "CC6633";
+ $detector{"type"}        = "Cons";
+  my $Rmin1 = 75;
+  my $Rmax1 = 140;
+  my $Rmin2 = 75;
+  my $Rmax2 = 140;
+  my $Dz    = 0.1;
+  my $Sphi  = 0;
+  my $Dphi  = 360;
+ $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+ $detector{"material"}    = "Vacuum";
+ $detector{"mfield"}      = "no";
+ $detector{"ncopy"}       = 1;
+ $detector{"pMany"}       = 1;
+ $detector{"exist"}       = 1;
+ $detector{"visible"}     = 1;
+ $detector{"style"}       = 0;
+ $detector{"sensitivity"} = "FLUX";
+ $detector{"hit_type"}    = "FLUX";
+ $detector{"identifiers"} = "id manual 3220000";
+ print_det(\%detector, $file);
+}
+make_largeangle_middle();
 
 sub make_largeangle_inner
 {
@@ -173,7 +298,7 @@ sub make_largeangle_inner
  $detector{"style"}       = 0;
  $detector{"sensitivity"} = "FLUX";
  $detector{"hit_type"}    = "FLUX";
- $detector{"identifiers"} = "id manual 3220000";
+ $detector{"identifiers"} = "id manual 3230000";
  print_det(\%detector, $file);
 }
 make_largeangle_inner();
@@ -204,7 +329,7 @@ sub make_largeangle_rear
  $detector{"style"}       = 0;
  $detector{"sensitivity"} = "FLUX";
  $detector{"hit_type"}    = "FLUX";
- $detector{"identifiers"} = "id manual 3230000";
+ $detector{"identifiers"} = "id manual 3240000";
  print_det(\%detector, $file);
 }
 make_largeangle_rear();
