acc65f56ff4f639e3f2414343b6c15f42b6ff7de
zwzhao <zwzhao@jlab.org>
2013-01-07 21:28:24 +0000

    add SIDIS large angle absorber

--- solid_gemc/database_io/geometry/solid_CLEO_SIDIS_absorber.pl
+++ solid_gemc/database_io/geometry/solid_CLEO_SIDIS_absorber.pl
@@ -23,7 +23,7 @@ use Math::Trig;
 
 my $DetectorMother="root";
 
-sub make_absorber
+sub make_absorber_forwarangle
 {
  my $material="Tungsten";
  my $z=140;
@@ -55,4 +55,38 @@ sub make_absorber
     $detector{"identifiers"} = "";
     print_det(\%detector, $file);
 }
-make_absorber();
+make_absorber_forwarangle();
+
+sub make_absorber_largeangle
+{
+ my $material="Tungsten";
+ my $z=70;
+
+    $detector{"name"}        = "$DetectorName\_largeangle";
+    $detector{"mother"}      = "$DetectorMother" ;
+    $detector{"description"} = $detector{"name"};
+    $detector{"pos"}        = "0*cm 0*cm $z*cm";
+    $detector{"rotation"}   = "0*deg 0*deg 0*deg";
+    $detector{"color"}      = "003300";
+    $detector{"type"}       = "Cons";
+    my $Rmin1 = 107.5;
+    my $Rmax1 = 140;
+    my $Rmin2 = 112.8;
+    my $Rmax2 = 140;
+    my $Dz    = 10;
+    my $Sphi  = 0;
+    my $Dphi  = 360;
+    $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+    $detector{"material"}   = "$material";
+    $detector{"mfield"}     = "no";
+    $detector{"ncopy"}      = 1;
+    $detector{"pMany"}       = 1;
+    $detector{"exist"}       = 1;
+    $detector{"visible"}     = 1;
+    $detector{"style"}       = 1;
+    $detector{"sensitivity"} = "no";
+    $detector{"hit_type"}    = "";
+    $detector{"identifiers"} = "";
+    print_det(\%detector, $file);
+}
+make_absorber_largeangle();
