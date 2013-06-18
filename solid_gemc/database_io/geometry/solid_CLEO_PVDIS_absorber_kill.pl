47081a01b7a2c9a0261ec112f72381d3ce1f47c5
zwzhao <zwzhao@jlab.org>
2013-06-18 18:16:19 +0000

    change absorber from a ring to a cone and  a ring to shield beamline more

--- solid_gemc/database_io/geometry/solid_CLEO_PVDIS_absorber_kill.pl
+++ solid_gemc/database_io/geometry/solid_CLEO_PVDIS_absorber_kill.pl
@@ -26,21 +26,55 @@ my $DetectorMother="root";
  my $material="Kryptonite";
  my $color="000000";
 
-sub make_absorber_forwarangle
+sub make_absorber_forwarangle_1
 {
- my $z=96;
+ my $z=95;
 
-    $detector{"name"}        = "$DetectorName\_forwarangle";
+    $detector{"name"}        = "$DetectorName\_forwarangle_1";
     $detector{"mother"}      = "$DetectorMother" ;
     $detector{"description"} = $detector{"name"};
     $detector{"pos"}        = "0*cm 0*cm $z*cm";
     $detector{"rotation"}   = "0*deg 0*deg 0*deg";
     $detector{"color"}      = "$color";
     $detector{"type"}       = "Cons";
-    my $Rmin1 = 7;
-    my $Rmax1 = 20;
-    my $Rmin2 = 7;
+    my $Rmin1 = 2;
+    my $Rmax1 = 5;
+    my $Rmin2 = 10;
     my $Rmax2 = 20;
+    my $Dz    = 45;
+    my $Sphi  = 0;
+    my $Dphi  = 360;
+    $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
+    $detector{"material"}   = "$material";
+    $detector{"mfield"}     = "no";
+    $detector{"ncopy"}      = 1;
+    $detector{"pMany"}       = 1;
+    $detector{"exist"}       = 1;
+    $detector{"visible"}     = 1;
+    $detector{"style"}       = 1;
+    $detector{"sensitivity"} = "no";
+    $detector{"hit_type"}    = "";
+    $detector{"identifiers"} = "";
+    print_det(\%detector, $file);
+}
+make_absorber_forwarangle_1();
+
+
+sub make_absorber_forwarangle_2
+{
+ my $z=145;
+
+    $detector{"name"}        = "$DetectorName\_forwarangle_2";
+    $detector{"mother"}      = "$DetectorMother" ;
+    $detector{"description"} = $detector{"name"};
+    $detector{"pos"}        = "0*cm 0*cm $z*cm";
+    $detector{"rotation"}   = "0*deg 0*deg 0*deg";
+    $detector{"color"}      = "$color";
+    $detector{"type"}       = "Cons";
+    my $Rmin1 = 10;
+    my $Rmax1 = 40;
+    my $Rmin2 = 10;
+    my $Rmax2 = 40;
     my $Dz    = 5;
     my $Sphi  = 0;
     my $Dphi  = 360;
@@ -57,37 +91,5 @@ sub make_absorber_forwarangle
     $detector{"identifiers"} = "";
     print_det(\%detector, $file);
 }
-make_absorber_forwarangle();
+make_absorber_forwarangle_2();
 
-# sub make_absorber_largeangle
-# {
-#  my $z=40;
-# 
-#     $detector{"name"}        = "$DetectorName\_largeangle";
-#     $detector{"mother"}      = "$DetectorMother" ;
-#     $detector{"description"} = $detector{"name"};
-#     $detector{"pos"}        = "0*cm 0*cm $z*cm";
-#     $detector{"rotation"}   = "0*deg 0*deg 0*deg";
-#     $detector{"color"}      = "$color";
-#     $detector{"type"}       = "Cons";
-#     my $Rmin1 = 43;
-#     my $Rmax1 = 100;
-#     my $Rmin2 = 43;
-#     my $Rmax2 = 100;
-#     my $Dz    = 1;
-#     my $Sphi  = 0;
-#     my $Dphi  = 360;
-#     $detector{"dimensions"}  = "$Rmin1*cm $Rmax1*cm $Rmin2*cm $Rmax2*cm $Dz*cm $Sphi*deg $Dphi*deg";
-#     $detector{"material"}   = "$material";
-#     $detector{"mfield"}     = "no";
-#     $detector{"ncopy"}      = 1;
-#     $detector{"pMany"}       = 1;
-#     $detector{"exist"}       = 1;
-#     $detector{"visible"}     = 1;
-#     $detector{"style"}       = 1;
-#     $detector{"sensitivity"} = "no";
-#     $detector{"hit_type"}    = "";
-#     $detector{"identifiers"} = "";
-#     print_det(\%detector, $file);
-# }
-# make_absorber_largeangle();
