a53a1234ccaa23f0fc34eac03fb4f68540e670fe
zwzhao <zwzhao@jlab.org>
2015-11-20 17:27:18 +0000

    modify id and convert new file

--- subsystem/ec/layout/EC_map.C
+++ subsystem/ec/layout/EC_map.C
@@ -1,15 +1,3 @@
-/*
-This script is used to get the fired module and its surrounded 6 module ID
-
-inputs are hit_x hit_y unit is cm
-module ID is given by column and row number of the module and the sector number(30 sectors in total).
-
-coordinate definition is in readme.txt
-
-written by Yuxiang Zhao (yxzhao@jlab.org)
-
-*/
-
 #include<iostream>
 #include<fstream>
 
@@ -22,16 +10,20 @@ void EC_map(string option,double hit_x=0,double hit_y=0){
 	//________________________________construct map_____________________________________________________
 	
 	ofstream OUTPUT_file;
-	if (option == "convert") {OUTPUT_file.open("EC_map_parameters.txt");}
+	if (option == "convert") {
+	  OUTPUT_file.open("map_FAEC_ANL_20130628_PVDIS_parameters.txt");
+	  OUTPUT_file << "idymax" << "\t" << "| " << 54 << "\t| - | - | - |  - | - | - | - | - " << endl;	  
+	}
 	
 	ifstream INPUT_file;
-	INPUT_file.open("EC_map_final.txt");
-	if(!INPUT_file)cout<<"ERROR!!! Can't open EC_map_final.txt"<<endl;
+	INPUT_file.open("map_FAEC_ANL_20130628.txt");
+	if(!INPUT_file)cout<<"ERROR!!! Can't open map_FAEC_ANL_20130628.txt"<<endl;	
 
 	int num_module_in_row[54]={0};    //54 layers in y direction
 	double y_bak[54]={100000};
 	double x[54][50]={100000};           
 	double y[54][50]={100000}; 
+	
 	
 	double total_EC_module=0;
 	for(int i=0;i<54;i++){ //54 rows
@@ -41,22 +33,62 @@ void EC_map(string option,double hit_x=0,double hit_y=0){
 		double tmp_y;
 		INPUT_file>>tmp_y;
 		y_bak[i]=tmp_y;       //make a backup in order to judge which row a certain particle hits the EC
-		if (option == "convert") {
-		  OUTPUT_file << Form("num_y%i",i) << "\t" << "| " << num_module_in_row[i] << "\t| - | - | - |  - | - | - | - | - " << endl;
-		  OUTPUT_file << Form("y%i",i) << "\t" << "| " << tmp_y << "\t| cm | - |  - | - | - | - | - | - " << endl;
-		}		  
 		for(int j=0;j<num_module_in_row[i];j++){
 			INPUT_file>>x[i][j];
 			y[i][j]=tmp_y;     // in each row , y coordinate is the same
-			if (option == "convert") {
-			  OUTPUT_file << Form("y%i_x%i",i,j) << "\t" << "| " << x[i][j] << "\t| cm | - |  - | - | - | - | - | - " << endl;
-			}		  			
 		}
 		//cout<<num_module_in_row[i]<<"	"<<y[i][0]<<endl;
 
 	}
 	
-	if (option == "convert") OUTPUT_file.close();
+	//__________deal with id______________________________________________________
+	int id[54][50]={100000};    //the structure is the same as x[54][50], y[54][50]   54 is the number of y rows
+	int counter_id=0;
+	for(int i=0;i<54;i++){
+		for(int j=0;j<num_module_in_row[i];j++){
+			counter_id++;
+			id[i][j]=counter_id;			
+// 			cout << id[i][j] << endl;
+		}
+	}	
+	//__________id is finished________________________
+	
+	//__________deal with separating EC into 30 sectors________________________
+	int sector[54][50]={100000};    //the structure is the same as x[54][50], y[54][50]   54 is the number of y rows
+	for(int i=0;i<54;i++){
+		for(int j=0;j<num_module_in_row[i];j++){
+			TVector2 vec(x[i][j],y[i][j]);
+			double phi_module=vec.Phi();
+			for(int k=0;k<30;k++){   //30 sectors
+				if(phi_module>=k*12.0/180.0*3.141592 && phi_module<(k+1.0)*12.0/180.0*3.141592){  //sector k
+					sector[i][j]=k+1;
+// 				    cout << sector[i][j] << endl;				
+				}
+			}
+		}
+	}
+
+	//_______sector map is finished___________________________________________	
+	
+	int status[54][50]={100000};    //the structure is the same as x[54][50], y[54][50]   54 is the number of y rows
+	for(int i=0;i<54;i++){
+		for(int j=0;j<num_module_in_row[i];j++){
+		  status[i][j]=1; //all active for PVDIS FAEC
+		}
+	}
+
+	if (option == "convert") {
+	  for(int i=0;i<54;i++){
+	      OUTPUT_file << Form("idxmax_idy%i",i+1) << "\t" << "| " << num_module_in_row[i] << "\t| - | - | - |  - | - | - | - | - " << endl;
+	      OUTPUT_file << Form("y_idy%i",i+1) << "\t" << "| " << tmp_y << "\t| cm | - |  - | - | - | - | - | - " << endl;
+		  
+		  for(int j=0;j<num_module_in_row[i];j++){
+		    OUTPUT_file << Form("x_idy%i_idx%i_id%i_sector%i_status%i",i+1,j+1,id[i][j],sector[i][j],status[i][j]) << "\t" << "| " << x[i][j] << "\t| cm | - |  - | - | - | - | - | - " << endl;
+		  }		  			
+	  }	
+	  OUTPUT_file.close();
+	}
+	
 /*
 	//check map here
 	int check_row=0;
@@ -75,10 +107,10 @@ void EC_map(string option,double hit_x=0,double hit_y=0){
 	
 	//outputs: fired module ID and its surrounded 6 modules ID
 	//total number of EC modules will be printed out
-	int hit_x_id;                   //once we get hit id, we can easily locate the hitted module coordinate
-	int hit_y_id;
-	int hit_around_x_id[6]={100000};      //around 6 modules, a variable label indicates how many surrounded modules are around the hitted module
-	int hit_around_y_id[6]={100000};
+	int hit_idx;                   //once we get hit id, we can easily locate the hitted module coordinate
+	int hit_idy;
+	int hit_around_idx[6]={100000};      //around 6 modules, a variable label indicates how many surrounded modules are around the hitted module
+	int hit_around_idy[6]={100000};
 	//___________________________________
 	
 	int first_y=0;
@@ -95,46 +127,46 @@ void EC_map(string option,double hit_x=0,double hit_y=0){
 		}
 	}
 	if( fabs(hit_y-y_bak[first_y])<=fabs(hit_y-y_bak[last_y]) ){
-		hit_y_id=first_y;                                 //found y id
+		hit_idy=first_y;                                 //found y id
 	}else{
-		hit_y_id=last_y;
+		hit_idy=last_y;
 	}
 
 	//then go and find x id
 	int first_x=0;
-	int last_x=num_module_in_row[hit_y_id]-1;   //last depends on which row the hit it is on
+	int last_x=num_module_in_row[hit_idy]-1;   //last depends on which row the hit it is on
 	while( last_x!=first_x+1 ){
 		int mid_x=(int)(first_x+last_x)/2;
-		if(hit_x>x[hit_y_id][mid_x]){
+		if(hit_x>x[hit_idy][mid_x]){
 			first_x=mid_x;
-		}else if(hit_x<x[hit_y_id][mid_x]){
+		}else if(hit_x<x[hit_idy][mid_x]){
 			last_x=mid_x;
 		}else{
 			first_x=mid_x;
 			last_x=first_x+1;
 		}
 	}
-	if( fabs(hit_x-x[hit_y_id][first_x])<=fabs(hit_x-x[hit_y_id][last_x]) ){
-		hit_x_id=first_x;            //found x id
+	if( fabs(hit_x-x[hit_idy][first_x])<=fabs(hit_x-x[hit_idy][last_x]) ){
+		hit_idx=first_x;            //found x id
 	}else{
-		hit_x_id=last_x;
+		hit_idx=last_x;
 	}
 
 /*
 	//check hit id here
-	cout<<"hit_x_id "<<hit_x_id<<endl;
-	cout<<"hit_y_id "<<hit_y_id<<endl;
+	cout<<"hit_idx "<<hit_idx<<endl;
+	cout<<"hit_idy "<<hit_idy<<endl;
 */
 	//__________________________________find the surrounded other 6 modules________________________
-	int tmp_x_id[15]={hit_x_id-7, hit_x_id-6, hit_x_id-5, hit_x_id-4, hit_x_id-3, hit_x_id-2, hit_x_id-1, hit_x_id, hit_x_id+1, hit_x_id+2, hit_x_id+3, hit_x_id+4, hit_x_id+5, hit_x_id+6, hit_x_id+7};
-	int tmp_y_id[3]={hit_y_id-1, hit_y_id, hit_y_id+1};
+	int tmp_idx[15]={hit_idx-7, hit_idx-6, hit_idx-5, hit_idx-4, hit_idx-3, hit_idx-2, hit_idx-1, hit_idx, hit_idx+1, hit_idx+2, hit_idx+3, hit_idx+4, hit_idx+5, hit_idx+6, hit_idx+7};
+	int tmp_idy[3]={hit_idy-1, hit_idy, hit_idy+1};
 	int label=0;
 	for(int i=0;i<15;i++){// x scan
 		for(int j=0;j<3;j++){  //y scan
-			if(tmp_y_id[j]>=0&&tmp_y_id[j]<54&&tmp_x_id[i]>=0&&tmp_x_id[i]<num_module_in_row[tmp_y_id[j]]&&(tmp_x_id[i]!=hit_x_id || tmp_y_id[j]!=hit_y_id) ){ // in range
-				if(sqrt( pow( (x[tmp_y_id[j]][tmp_x_id[i]]-x[hit_y_id][hit_x_id]),2 )+ pow((y[tmp_y_id[j]][tmp_x_id[i]]-y[hit_y_id][hit_x_id]),2 ))<15.0){
-					hit_around_x_id[label]=tmp_x_id[i];
-					hit_around_y_id[label]=tmp_y_id[j];
+			if(tmp_idy[j]>=0&&tmp_idy[j]<54&&tmp_idx[i]>=0&&tmp_idx[i]<num_module_in_row[tmp_idy[j]]&&(tmp_idx[i]!=hit_idx || tmp_idy[j]!=hit_idy) ){ // in range
+				if(sqrt( pow( (x[tmp_idy[j]][tmp_idx[i]]-x[hit_idy][hit_idx]),2 )+ pow((y[tmp_idy[j]][tmp_idx[i]]-y[hit_idy][hit_idx]),2 ))<15.0){
+					hit_around_idx[label]=tmp_idx[i];
+					hit_around_idy[label]=tmp_idy[j];
 					label++;
 				}
 			}
@@ -142,40 +174,23 @@ void EC_map(string option,double hit_x=0,double hit_y=0){
 	}
 /*
 	cout<<"find  "<<label<<" around the hitted module"<<endl;
-	cout<<"x_id	y_id"<<endl;
-	cout<<hit_x_id<<"	"<<hit_y_id<<endl;
+	cout<<"idx	idy"<<endl;
+	cout<<hit_idx<<"	"<<hit_idy<<endl;
 	for(int l=0;l<label;l++){
-		cout<<hit_around_x_id[l]<<"	"<<hit_around_y_id[l]<<endl;
+		cout<<hit_around_idx[l]<<"	"<<hit_around_idy[l]<<endl;
 	}
 
 */
 	//______________________________________module search is finished ____________________________________________________________________
-	
-	
-	//__________deal with separating EC into 30 sectors______________________________________________________
-	double sector_id[54][50]={100000};    //the structure is the same as x[54][50], y[54][50]   54 is the number of y rows
-	for(int i=0;i<54;i++){
-		for(int j=0;j<num_module_in_row[i];j++){
-			TVector2 vec(x[i][j],y[i][j]);
-			double phi_module=vec.Phi();
-			for(int k=0;k<30;k++){   //30 sectors
-				if(phi_module>=k*12.0/180.0*3.141592 && phi_module<(k+1.0)*12.0/180.0*3.141592){  //sector k
-					sector_id[i][j]=k;
-				}
-			}
-		}
-	}
 
-	//_______sector map is finished_______________________________________________________________________________
+	
 	cout<<"________total EC module: "<<total_EC_module<<endl;
 	cout<<"find  "<<label<<" around the hitted module"<<endl;
-	cout<<"x_id	y_id	sector_id"<<endl;
-	cout<<hit_x_id<<"	"<<hit_y_id<<"	"<<sector_id[hit_y_id][hit_x_id]<<" ----> this module is hitted!"<<endl;
+	cout<<"idy\tidx\tid\tsector"<<endl;
+	cout<<hit_idy+1<<"\t"<<hit_idx+1<<"\t"<<id[hit_idy][hit_idx]<<"\t"<<sector[hit_idy][hit_idx]<<" ----> this module is hitted!"<<endl;
 	for(int l=0;l<label;l++){
-		cout<<hit_around_x_id[l]<<"	"<<hit_around_y_id[l]<<"	"<<sector_id[hit_around_y_id[l]][hit_around_x_id[l]]<<endl;
+	  cout<<hit_around_idy[l]+1<<"\t"<<hit_around_idx[l]+1<<"\t"<<id[hit_around_idy[l]][hit_around_idx[l]]<< "\t"<<sector[hit_around_idy[l]][hit_around_idx[l]]<<endl;
 	}
-
-
 
 }
 
--- subsystem/ec/layout/readme.txt
+++ subsystem/ec/layout/readme.txt
@@ -1,10 +1,38 @@
-EC coordinate :  y+ up, x+ left
+From the drawing and tables from engineers, we got the EC hexagon module center coordinate in lab frame and save as txt file
 
-in y direction: 54 rows
-in x direction: 46 coll. in maximum row
+EC_map.C script is used to 
+1. convert the EC coordinate map into GEMC parameter file
+2. get the fired module and its surrounded 6 module ID with hit position hit_x hit_y in cm
 
+the SoLID lab frame
+*********************
+The solenoid coil central plane and solenoid central line crosses at the origin.
+electron beam and solenoid central line are along +z axis
+y axis is vertical and +y pointing up relative to the ground
+x axis is horizontal and +x pointing left relative to the electron beam
+*********************
+refer to https://hallaweb.jlab.org/wiki/index.php/Solid_Software#Coordinate_System
 
-coordinates of each module is recorded in x[i][j], y[i][j]  
-i: module id along y direction that's 0-->53      
-j: module id along x direction, range is depend on which row it is
-[0][0] module is the most bottom right module.
+Imaging we are riding along the beam and facing EC from front
+
+EC module ID consist of idx,idy,id,sector
+idx and idy are given by column and row number of the module
+Their values increase from bottom to top for idy, from right to left for idx
+idx=0 and idy=0 module is the most bottom right module.
+In the script, module center coordinate in lab frame is recorded in x[idy][idx], y[idy][idx]
+id is the total id of the module, its value increase by following idx first, then idy
+sector is the sector number, it matches baffle for FAEC
+
+for FAEC, the range are
+sector: 1-30 
+id:1-1748
+idy: 1-54 rows
+idx direction: 1-46 coll. in maximum row
+
+keyword status is 1 for module existing or 0 for not, this is to ensure PVDIS and SIDIS have same module id
+
+= log ==========
+
+written by Yuxiang Zhao (yxzhao@jlab.org)  2014/08
+
+Update by Zhiwen Zhao  (zwzhao@jlab.org)  2015/11
