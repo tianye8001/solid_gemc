1d32c740a2343a28bf2b51bcabd475b3b4ea213c
zwzhao <zwzhao@jlab.org>
2017-09-18 21:49:21 +0000

    update analysys code

--- subsystem/hgc/analysis/compare_1D.C
+++ subsystem/hgc/analysis/compare_1D.C
@@ -232,74 +232,6 @@ gStyle->SetOptStat(0);
 // "full,#pi-,z=0cm,#theta=14.8deg",
 // };
 
-    const int n=4;
-    const int m=13;
-char* input_filename[n][m]={
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p2.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p2.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p3.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p3.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p4.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p4.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p5.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p5.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p6.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p6.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p7.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p7.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p8.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p2.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p2.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p3.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p3.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p4.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p4.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p5.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p5.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p6.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p6.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p7.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p7.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim/output_p8.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p2.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p2.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p3.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p3.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p4.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p4.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p5.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p5.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p6.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p6.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p7.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p7.5_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p8.0_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",
-
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p2.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p2.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p3.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p3.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p4.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p4.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p5.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p5.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p6.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p6.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p7.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p7.5_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-"data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p8.0_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",
-
-
-};
-
-char* label[n]={
-"alone,#pi-,z=0cm,#theta=8deg",
-"alone,#pi-,z=0cm,#theta=14.8deg old",    
-"alone,#pi-,z=0cm,#theta=8deg",    
-"alone,#pi-,z=0cm,#theta=14.8deg old",    
-};
 
 // char* label[m]={
 // };
@@ -329,18 +261,138 @@ char* label[n]={
 // };
 // int color[m]={2,3};
 
+
+// const int n=4;
+// const int m=13;  
+// char* var[m]={"2.0","2.5","3.0","3.5","4.0","4.5","5.0","5.5","6.0","6.5","7.0","7.5","8.0"};    
+// string input_filename[n][m];    
+// for(int i=0;i<m;i++) {
+//   char name[200];
+//   sprintf(name,Form("data/gemc2.1_geant4.9.6.p03_alone_pim/output_p%s_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",var[i]));   
+//   input_filename[0][i]=name;
+//   sprintf(name,Form("data/gemc2.1_geant4.9.6.p03_alone_pim/output_p%s_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",var[i]));   
+//   input_filename[1][i]=name;
+//   sprintf(name,Form("data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p%s_z0_theta8_phi0_blockoff_fieldon_1e3_output.root",var[i]));   
+//   input_filename[2][i]=name;
+//   sprintf(name,Form("data/gemc2.1_geant4.9.6.p03_alone_pim_old/output_p%s_z0_theta14.8_phi0_blockoff_fieldon_1e3_output.root",var[i]));   
+//   input_filename[3][i]=name;
+// }
+// 
+// char* label[n]={
+// "alone,#pi-,z=0cm,#theta=8deg",
+// "alone,#pi-,z=0cm,#theta=14.8deg old",    
+// "alone,#pi-,z=0cm,#theta=8deg",    
+// "alone,#pi-,z=0cm,#theta=14.8deg old",    
+// };
+
+const int m=11;  
+char* var[m]={"2.5","3.0","3.5","4.0","4.5","5.0","5.5","6.0","6.5","7.0","7.5"};    
+
+// const int n=6;
+// string input_filename[n][m];
+
+// for(int i=0;i<m;i++) {
+//   char name[200]; 
+// 
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[0][i]=name;  
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[1][i]=name;
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+//   input_filename[2][i]=name;  
+//   
+// 
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmoveNO_coneYES/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+//   input_filename[3][i]=name;     
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmoveNO_coneYES/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[4][i]=name;
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmoveNO_coneYES/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+//   input_filename[5][i]=name;    
+//   
+// }
+// char* label[n]={
+// "(new) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",  
+// "(new) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+// "(new) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+// "(old) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",
+// "(old) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+// "(old) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+// };
+// int MarkerStyle[n]={4,4,4,8,8,8};
+
+// const int n=6;
+// string input_filename[n][m];
+// for(int i=0;i<m;i++) {
+//   char name[200]; 
+// 
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[0][i]=name;  
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[1][i]=name;
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+//   input_filename[2][i]=name;  
+//   
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03_WLS_meas/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[3][i]=name;  
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03_WLS_meas/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+//   input_filename[4][i]=name;
+//   sprintf(name,Form("data/JLAB_VERSION_1.2/pmtmove10cm_cone10cmtilt65deg/QE_H12700_03_WLS_meas/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+//   input_filename[5][i]=name;  
+// }
+// char* label[n]={
+// "(uncoated) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",  
+// "(uncoated) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+// "(uncoated) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+// "(coated) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",
+// "(coated) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+// "(coated) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+// };
+// int MarkerStyle[n]={4,4,4,26,26,26};
+// int color[n]={1,2,3,1,2,3};
+
+const int n=6;
+string input_filename[n][m];
+for(int i=0;i<m;i++) {
+  char name[200]; 
+
+  sprintf(name,Form("data/JLAB_VERSION_1.3/pmtmove10cm_cone10cmtilt65deg/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+  input_filename[0][i]=name;  
+  sprintf(name,Form("data/JLAB_VERSION_1.3/pmtmove10cm_cone10cmtilt65deg/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+  input_filename[1][i]=name;
+  sprintf(name,Form("data/JLAB_VERSION_1.3/pmtmove10cm_cone10cmtilt65deg/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+  input_filename[2][i]=name;  
+  
+  sprintf(name,Form("data/JLAB_VERSION_1.3/new/output_pim_z0_p%s_theta7.5_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+  input_filename[3][i]=name;  
+  sprintf(name,Form("data/JLAB_VERSION_1.3/new/output_pim_z0_p%s_theta8.0_phi0_blockoff_fieldoff_1e3_output.root",var[i]));  
+  input_filename[4][i]=name;
+  sprintf(name,Form("data/JLAB_VERSION_1.3/new/output_pim_z0_p%s_theta14.8_phi0_blockoff_fieldoff_1e3_output.root",var[i]));   
+  input_filename[5][i]=name;  
+}
+char* label[n]={
+"(current) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",  
+"(current) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+"(current) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+"(shift20cmdown) alone,#pi-,field off,block off,z=0cm,#theta=7.5deg,#phi=0deg",
+"(shift20cmdown) alone,#pi-,field off,block off,z=0cm,#theta=8.0deg,#phi=0deg",
+"(shift20cmdown) alone,#pi-,field off,block off,z=0cm,#theta=14.8deg,#phi=0deg",  
+};
+int MarkerStyle[n]={4,4,4,26,26,26};
+int color[n]={1,2,3,1,2,3};
+
+
+
 TH1F *hcount_total_p[n];
 for(int j=0;j<n;j++){
-hcount_total_p[j]=new TH1F(Form("hcount_total_p_%i",j),"photoelectron;P (GeV);count",13,1.75,8.25);
+// hcount_total_p[j]=new TH1F(Form("hcount_total_p_%i",j),"photoelectron;P (GeV);count",m,1.75,8.25);
+hcount_total_p[j]=new TH1F(Form("hcount_total_p_%i",j),";P (GeV);photoelectron count (sim*0.5)",m,2.25,7.75);  
 // hcount_total_p->SetAxisRange(2,8,"X");
 hcount_total_p[j]->SetAxisRange(0,50,"Y");   
-hcount_total_p[j]->SetMarkerStyle(4);
-// hcount_total_p[j]->SetMarkerSize(0.3);
-hcount_total_p[j]->SetMarkerColor(j+1);
-hcount_total_p[j]->SetLineColor(j+1);
+hcount_total_p[j]->SetMarkerStyle(MarkerStyle[j]);
+hcount_total_p[j]->SetMarkerSize(2);
+hcount_total_p[j]->SetMarkerColor(color[j]);
+hcount_total_p[j]->SetLineColor(color[j]);
 }
-
-
 
 
 TCanvas *c = new TCanvas("compare","compare",1600,800);
@@ -350,15 +402,17 @@ TH1F *h[n][m];
 TLegend* leg[n];
 for(int j=0;j<n;j++){
   c->cd(j+1);  
-  leg[j]= new TLegend(0.7, 0.9-0.05*m, 0.95, 0.95);  
+  leg[j]= new TLegend(0.7, 0.95-0.05*m, 0.95, 0.95);  
 for(int i=0;i<m;i++){
+//   cout << j << " " << i << endl;  
 //     if(i==3 || i>6) continue;
-  input[j][i]=new TFile(input_filename[j][i]);
+  input[j][i]=new TFile(input_filename[j][i].c_str());
+//   cout << " " << input_filename[j][i] << endl;
   if (input[j][i]->IsZombie()) {
     cout << "Error opening ratefile " << input_filename[j][i] << endl;
     exit(-1);
   }
-  else cout << "open file " << input_filename[j][i] << endl;
+  else cout << "ok open file " << input_filename[j][i] << endl;
   
   
 //   char hstname[100];
@@ -391,12 +445,12 @@ leg[j]->Draw();
 
 
 TLegend* legend;
-  legend= new TLegend(0.5, 0.9-0.05*n, 0.95, 0.95);  
+  legend= new TLegend(0.5, 0.98-0.05*n, 0.95, 0.98);  
 TCanvas *c1 = new TCanvas("c","c",1000,800);
 for(int j=0;j<n;j++){
 if (j==0) hcount_total_p[j]->Draw("E1");
-else hcount_total_p[j]->Draw("E1 same");
-  legend->AddEntry(hcount_total_p[j], Form("%s",label[j]),"l");    
+else hcount_total_p[j]->Draw("E same");
+  legend->AddEntry(hcount_total_p[j], Form("%s",label[j]),"lep");    
 }
 legend->Draw();
 
