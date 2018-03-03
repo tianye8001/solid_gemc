f80020f07347d1885ea9008940a77e499761ee35
zwzhao <zwzhao@jlab.org>
2018-03-03 21:41:16 +0000

    test new EC hit processing

--- subsystem/ec/ec_segmented_solid_gemc/solid_ec_bank.pl
+++ subsystem/ec/ec_segmented_solid_gemc/solid_ec_bank.pl
@@ -45,7 +45,43 @@ sub define_bank
 	insert_bank_variable(\%configuration, $bankname, "mvy",    21,   "Dd", "y component of primary vertex of the mother of the particle entering the sensitive volume");
 	insert_bank_variable(\%configuration, $bankname, "mvz",    22,   "Dd", "z component of primary vertex of the mother of the particle entering the sensitive volume");
 	insert_bank_variable(\%configuration, $bankname, "avg_t",  23,   "Dd", "Average time");
-	insert_bank_variable(\%configuration, $bankname, "id",     24,   "Di", "id number");		
+	insert_bank_variable(\%configuration, $bankname, "nsteps",  24,   "Di", "Number of geant4 steps");
+	insert_bank_variable(\%configuration, $bankname, "procID",  25,   "Di", "Process that created the first particle");
+	insert_bank_variable(\%configuration, $bankname, "totEdepB",  26,   "Dd", "");	
+	insert_bank_variable(\%configuration, $bankname, "totEend",  27,   "Dd", "");	
+	
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg0",  30,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg1",  31,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg2",  32,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg3",  33,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg4",  34,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg5",  35,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg6",  36,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg7",  37,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg8",  38,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Edep_seg9",  39,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg0",  40,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg1",  41,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg2",  42,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg3",  43,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg4",  44,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg5",  45,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg6",  46,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg7",  47,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg8",  48,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "EdepB_seg9",  49,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg0",  50,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg1",  51,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg2",  52,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg3",  53,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg4",  54,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg5",  55,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg6",  56,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg7",  57,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg8",  58,   "Dd", "");		
+	insert_bank_variable(\%configuration, $bankname, "Eend_seg9",  59,   "Dd", "");		
+	
+	insert_bank_variable(\%configuration, $bankname, "id",     98,   "Di", "id number");		
 	insert_bank_variable(\%configuration, $bankname, "hitn",   99,   "Di", "Hit Number");
 }
 define_bank();
