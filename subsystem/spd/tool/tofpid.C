f6a3bd1917c614b12a67be61fe458a864f74162d
zwzhao <zwzhao@jlab.org>
2018-11-29 21:38:28 +0000

    minor change

--- subsystem/spd/tool/tofpid.C
+++ subsystem/spd/tool/tofpid.C
@@ -1,19 +1,19 @@
 {  
 // double d=266;    //(315-65)/cos(20/180*3.1415926)=266cm   TCS LA
 // double d=741;       //(315+415)/cos(10/180*3.1415926)=741cm  TCS FA
-// double d=770;       //(350+415)/cos(7.5/180*3.1415926)=770cm  SIDIS FA
-  double d=850;
+double d=786;       //(350+430)/cos(7/180*3.1415926)=786cm  SIDIS FA
+//   double d=850;
   
 const double M_pi=0.140,M_k=0.494,M_p=0.938;
-TF1 *ppi=new TF1("ppi","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,10);
+TF1 *ppi=new TF1("ppi","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,12);
 ppi->SetParameters(M_p,M_pi,d);
-TF1 *pk=new TF1("kp","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,10);
+TF1 *pk=new TF1("kp","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,12);
 pk->SetParameters(M_p,M_k,d);
-TF1 *kpi=new TF1("kpi","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,10);
+TF1 *kpi=new TF1("kpi","[2]/30./(x/sqrt(x*x+[0]*[0]))-[2]/30./(x/sqrt(x*x+[1]*[1]))",0,12);
 kpi->SetParameters(M_k,M_pi,d);
-ppi->SetMaximum(0.5);
+ppi->SetMaximum(7);
 ppi->SetMinimum(0);
-ppi->SetTitle("TOF PID;Mom (GeV/c);Time Diff (#mus)");
+ppi->SetTitle("TOF PID;Mom (GeV/c);Time Diff (ns)");
 ppi->SetLineColor(kBlack);
 ppi->SetLineStyle(1);
 ppi->Draw();
@@ -24,11 +24,16 @@ kpi->SetLineColor(kBlack);
 kpi->SetLineStyle(7);
 kpi->Draw("same");
 
-//assume 5sigma and 80ps resolution
-// TF1 *line=new TF1("line","0.6",0,10);
-TF1 *line=new TF1("line","0.1",0,10);
+TF1 *line=new TF1("line","5",0,12);
+// TF1 *line=new TF1("line","0.08",0,10);
 line->SetLineColor(kRed);
 line->Draw("same");
+
+TF1 *line1=new TF1("line1","0.4",0,12);
+// TF1 *line=new TF1("line","0.08",0,10);
+line1->SetLineColor(kRed);
+line1->Draw("same");				
+
 
 //       TLine *lh;
 //       TLatex  t;
